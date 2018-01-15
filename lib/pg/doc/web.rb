151c9b00f99ac79bf4b5edee39f0891826fc57a4
Kenaniah Cerny <kenaniah@gmail.com>
2018-01-15 10:30:52 -0800

    checkpoint

--- lib/pg/doc/web.rb
+++ lib/pg/doc/web.rb
@@ -3,8 +3,9 @@ require "sinatra/base"
 module PG
   module Doc
     class Web < Sinatra::Base
+      set :views, Proc.new { File.join(root, "../../../views") }
       get '/' do
-        halt 'Hello world!'
+        erb :index
       end
     end
   end
