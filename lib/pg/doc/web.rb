ac75f02b6a851933d11cd71ed1a9ffc3b8521000
Kenaniah Cerny <kenaniah@gmail.com>
2018-01-15 16:25:22 -0800

    checkpoint

--- lib/pg/doc/web.rb
+++ lib/pg/doc/web.rb
@@ -6,15 +6,27 @@ module PG
     # Returns a new web class with the connection already set
     def self.Web connection
       PG::Doc::Web.new do |app|
-        app.set_connection connection
+        app.setup connection
       end
     end
 
     class Web < Sinatra::Base
 
       # Sets the database connection to be used
-      def set_connection connection
+      def setup connection
         @conn = PG.connect connection
+        @cache = Hash.new
+        @cache[:tables] = @conn.exec(<<~SQL).values.group_by{ |row| row[0] }
+          SELECT
+            table_schema, table_name
+          FROM
+            information_schema.tables
+          WHERE
+            table_schema NOT ILIKE 'pg_%'
+            AND table_schema != 'information_schema'
+          ORDER BY
+            1, 2
+        SQL
       end
 
       set :views, Proc.new { File.join(root, "../../../views") }
